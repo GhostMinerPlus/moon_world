009e1d7dcf43000cb01b5abd8dac5b7a01bb464b
ghostminer <1696415745@qq.com>
2024-09-13 18:20:53 +0800

    场景编辑 便利

--- src/engine/builder.rs
+++ src/engine/builder.rs
@@ -2,23 +2,26 @@ use std::sync::Arc;
 
 use edge_lib::engine::EdgeEngine;
 use sqlite_dm::SqliteDataManager;
+use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 
 use super::{BodyBuilder, Joint};
 
 pub struct SceneBuilder {
-    body_v: Vec<BodyBuilder>,
-    joint_v: Vec<Joint>,
+    body: Vec<BodyBuilder>,
+    joint: Vec<Joint>,
     event_handler: Vec<String>,
     step_handler: Vec<String>,
     collision_handler: Vec<String>,
 }
 
 impl SceneBuilder {
-    pub async fn from_data(file: &str) -> Self {
-        let dm = Arc::new(SqliteDataManager::from_file("test.db", None).await);
+    pub async fn from_file(file: &str) -> Self {
+        let pool = SqlitePool::connect_with(SqliteConnectOptions::new().filename(file))
+            .await
+            .unwrap();
+        let dm = Arc::new(SqliteDataManager::new(pool, None));
         let mut engine = EdgeEngine::new(dm, "root").await;
 
-        
         todo!()
     }
 }
